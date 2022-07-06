dbf9e4c3ed1c37b83d0e51f033b68d965e23c899
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-06 19:04:54 +0900

    asd

--- HTML/ex/server.js
+++
@@ -1,31 +0,0 @@
-    const express = require('express');
-    const app = express();
-    const fs = require('fs');
-    const { SerialPort } = require('serialport');
-    const port = new SerialPort({
-      path: 'COM6',
-      baudRate: 9600
-    });
-    port.on('open', (err) => {
-      if(!err)
-        console.log('Serial Port Open');
-      else
-        console.error(err);
-    });
-    app.get('/', (req, res) => {
-      fs.readFile('./main.html', (err, data) => {
-        res.end(data);
-      });
-    });
-    app.get('/led-on', (req, res) => {
-      port.write([1]);
-      res.redirect('/');
-    });
-    app.get('/led-off', (req, res) => {
-      port.write([0]);
-      res.redirect('/');
-    });
-    app.listen(3030, () => {
-      console.log('Server Open: 3030 port');
-    });
-
