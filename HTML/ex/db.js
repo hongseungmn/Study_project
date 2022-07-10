8fe91f3596f9d58a669da61563092ee10674fcf1
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-10 17:16:42 +0900

    실시간 검색

--- HTML/ex/db.js
+++ HTML/ex/db.js
@@ -8,19 +8,21 @@ module.exports = class DB {
     };
 
     getName(conn, res, body) {
-    console.log(body);
-    const id = body.id;
-    const qry = 'select SN_NAME from TB_TEST where SN_ID = ?';
-    conn.query(qry, [id], (err, row) => {
-        console.log(row);
-        res.send(row);
-        res.end();
-    })
+        console.log("body =>>>>>>>>>>>>>>>>>>",body);
+        const id = body.keyWord;
+        //const qry = 'select SN_NAME from TB_TEST where SN_ID = ?';
+        const qry = "SELECT SN_NAME FROM TB_TEST WHERE SN_NAME LIKE '%"+id+"%'";
+        conn.query(qry, [id], (err, row) => {
+            console.log("row=>>>>>>>>>>>>>>>>>>>>>>",row);
+            res.send(row);
+            res.end();
+        })
     };
 
     getAll(conn,res){
         const qry = 'select * from TB_TEST';
         conn.query(qry,(err,row) => {
+            console.log("row=>>>>>>>>>>>>>>>>>>>>>>",row);
             res.send(row);
             res.end();
         })
