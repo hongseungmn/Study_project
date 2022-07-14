7c653c92d72eb52ce80ebb2a81c3730cd6573bdc
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-14 13:43:43 +0900

    python

--- python/Part_12(실습)/ex02.py
+++ python/Part_12(실습)/ex02.py
@@ -3,4 +3,21 @@
 from tkinter import *
 
 window = Tk()
-photo = PhotoImage(file=".//cherry.jpg")
+#PhotoImage 클래스는 jpg 확장자를 지원을 하지 않는다.
+photo = PhotoImage(file="./imageTest.png")
+#이미지는 들어가 잇는 레이블은 윈도우의 우측 배치
+lbl = Label(window, image=photo)
+lbl.photo = photo
+lbl.pack(side=RIGHT) 
+msg = "안녕하세요. 삶이 그대를 속일지라도 슬퍼하거나 노여워하지 마라."\
+        "안녕하세요. 삶이 그대를 속일지라도 슬퍼하거나 노여워하지 마라."\
+        "안녕하세요. 삶이 그대를 속일지라도 슬퍼하거나 노여워하지 마라."
+        
+# 텍스트를 출력
+lbl2 = Label(window,
+                justify=LEFT, #텍스트를 왼쪽정렬
+                padx=10,
+                text=msg).pack() 
+
+window.mainloop()
+
