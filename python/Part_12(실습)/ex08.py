bf6d0cff77b54d50466c4ed2dabd453cc5b552b1
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-14 22:04:29 +0900

    Create ex08.py

---
+++ python/Part_12(실습)/ex08.py
@@ -0,0 +1,22 @@
+# Canvas 의 메서드 
+#coords() : 좌표를 변경하는 메서드
+#itemconfig() : 색상을 변경하는 메서드
+# delete() : 선, 도형 등을 삭제하는 메서드
+
+
+from tkinter import *
+
+window = Tk()
+canvas = Canvas(window,width=500,height=500,bg="white")
+canvas.pack()
+
+#선을 그려본다
+#선에서 fill은 색상이고, width는 선의 굵기가 된다.
+line1 = canvas.create_line(0,0,500,500,fill="BLUE")
+line2 = canvas.create_line(0,0,500,0,fill="RED",width=5)
+#line1의 좌표를 변경한다
+canvas.coords(line1,250,0,250,0)
+
+rect1 = canvas.create_rectangle(50,50,200,200,fill="yellow")
+rect2 = canvas.create_rectangle(200,200,300,400,fill="red",outline="blue",width=10)
+window.mainloop()
