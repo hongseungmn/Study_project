afcbd9ea29ea9d0ac2e9b48981c97d36fead9443
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-14 22:00:19 +0900

    Update ex07.py

--- python/Part_12(실습)/ex07.py
+++ python/Part_12(실습)/ex07.py
@@ -8,6 +8,10 @@ canvas = Canvas(window,width=500,height=500,bg="white")
 canvas.pack()
 
 #선을 그려본다
+#선에서 fill은 색상이고, width는 선의 굵기가 된다.
 canvas.create_line(0,0,500,500,fill="BLUE")
 canvas.create_line(0,0,500,0,fill="RED",width=5)
+
+#사각형을 그려본다.
+canvas.create_rectangle(50,50,200,200,fill="yellow")
 window.mainloop()
