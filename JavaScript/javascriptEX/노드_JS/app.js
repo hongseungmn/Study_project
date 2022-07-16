c7b13f9f6e0cae20166f43ba33da750c1215fc9e
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-17 00:02:50 +0900

    update_package

--- JavaScript/javascriptEX/노드_JS/app.js
+++ JavaScript/javascriptEX/노드_JS/app.js
@@ -6,7 +6,20 @@ app.listen(3000,function() {
     console.log("Start! express server on port 3000!");
 });
 
+app.use(express.static("public"));
+
+//url routing
 app.get("/",function(req,res){
     //res.send("<h1>hi friend!</h1>");
     res.sendFile(__dirname + "/public/main.html");
 });
+
+app.get("/main",function(req,res){
+    //res.send("<h1>hi friend!</h1>");
+    res.sendFile(__dirname + "/public/main.html");
+});
+
+app.post("/email_post", function(req,res){
+    //get : req.paran('email')
+    res.send("post response");
+})
