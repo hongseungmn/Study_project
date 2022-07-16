e20b227af0c2b0b1268390d6fd5c7334d73df5c9
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-16 23:50:52 +0900

    노드_백엔드

--- JavaScript/javascriptEX/노드_JS/app.js
+++ JavaScript/javascriptEX/노드_JS/app.js
@@ -4,4 +4,9 @@ let app = express();
 
 app.listen(3000,function() {
     console.log("Start! express server on port 3000!");
-})
+});
+
+app.get("/",function(req,res){
+    res.send("<h1>hi friend!</h1>");
+    res.sendFile(__dirname + "public/main.html");
+});
