ce573350ff91e5148f4eb97921aa2b954e13b412
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-18 11:20:55 +0900

    Create ex07.py

---
+++ python/Part_13(실습)/ex07.py
@@ -0,0 +1,28 @@
+# for 문
+
+# infile = open("/Users/hongseongmin/Documents/GitHub/Study_project/python/Part_13(실습)/예제/test.txt")
+# for line in infile:
+#     print(line)
+    
+# 단어를 분리하기
+# infile = open("/Users/hongseongmin/Documents/GitHub/Study_project/python/Part_13(실습)/예제/test.txt","r")
+# for line in infile:
+#     line = line.rstrip() # 오른쪽 공백 문자를 제거
+#     word_list = line.split() # 단어들 분리
+#     for word in word_list:
+#         print(word)
+
+# infile.close()
+
+# 구분자를 지정해서 단어를 출력
+# line = "apple,grape,banana,pear"
+# word_list = line.split(",")
+# print(word_list)
+
+# 1문자씩 출력하기
+infile = open("/Users/hongseongmin/Documents/GitHub/Study_project/python/Part_13(실습)/예제/test.txt","r")
+ch = infile.read(1)
+while ch != "":
+    print(ch)
+    ch = infile.read(1)
+infile.close()
