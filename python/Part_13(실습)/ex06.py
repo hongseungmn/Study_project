8fd154f635c8079681df6bec6747ed345c7b6c00
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-18 10:36:54 +0900

    Update ex06.py

--- python/Part_13(실습)/ex06.py
+++ python/Part_13(실습)/ex06.py
@@ -1,2 +1,13 @@
 # 파이썬 파일 현재 위치 확인 및 변경을 하는 실습
 # tell() : 파일의 현재 위치를 확인할 때 사용한다.
+# seek() : 파일 현재 위치를 변경할 때 사용한다.
+# seek(0) : 파일의 처음 위치로 돌아간다
+
+file = open("/Users/hongseongmin/Documents/GitHub/Study_project/python/Part_13(실습)/예제/test.txt","r")
+print("파일포인터의 현재 위치 : ", file.tell())
+print(file.read())
+print("파일 포인터의 현재 위치 : ",file.tell())
+
+
+print("-----------------------------------------------")
+file.seek(0) # 맨 처음 위치로 포인터를 이동
