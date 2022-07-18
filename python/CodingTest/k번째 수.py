c4b086b336e820edc62aa0c99088a5168b52f245
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-18 20:35:47 +0900

    Update k번째 수.py

--- python/CodingTest/k번째 수.py
+++ python/CodingTest/k번째 수.py
@@ -1,16 +1,14 @@
 def solution(array, commands):
     answer = []
-    list_a = []
     for item in commands:
-        list_a = array[item[0]:item[1]]
+        #answer.append(sorted(array[item[0]-1:item[1]])[item[2]])
+        answer.append(sorted(array[item[0]-1:item[1]])[item[2]-1])
         
-        list_a = list_a.sort()
-        answer.append(list_a[item[2]])
-    
-    
     return answer
 
 array = [1, 5, 2, 6, 3, 7, 4]	
 commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
-solution(array,commands)
+answer = solution(array,commands)
+print(answer)
+
