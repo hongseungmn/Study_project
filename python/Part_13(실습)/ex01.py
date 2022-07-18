a6b84d4f213d5ceeeb2a2f6f568bc949773120ba
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-18 10:01:10 +0900

    Update ex01.py

--- python/Part_13(실습)/ex01.py
+++ python/Part_13(실습)/ex01.py
@@ -1,4 +1,14 @@
 # 파일은 데이터를 영구적으로 저장하는 형태이다.
 # 파일을 다루는 방법에 대해 알아보자
 # 1. 파일을 열기
+# open()내장 함수는 파일을 열고자 할 때 사용하는 함수이다.
+file = open("/Users/hongseongmin/Documents/GitHub/Study_project/python/Part_13(실습)/test.txt","r",encoding="UTF-8")
+# readline()함수는 파일에서 내용을 읽어오는데 줄단위로 읽어온다
+# 개행문자(\n)까지 가져온다.
+line = file.readline().rstrip()
+while line != "":
+    print(line)
+    # rstrip()는 오른쪽에 공백을 제거하는 함수인데 \n \t등을 다 제거를 해준다.
+    line = file.readline().rstrip()
 
+file.close()
