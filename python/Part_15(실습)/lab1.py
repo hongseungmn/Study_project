ae6e1696c5af2c9f3f30e1822a91ff1c4d77cf32
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-26 22:18:49 +0900

    Update lab1.py

--- python/Part_15(실습)/lab1.py
+++ python/Part_15(실습)/lab1.py
@@ -4,3 +4,26 @@
 # 출력결과
 # 1 1 2 3 5 8 13 21 34 ...
 
+class FibIterator(object):
+    def __init__(self, a=1, b=0, maxValue=50):
+        self.a = a
+        self.b = b
+        self.maxValue = maxValue
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        n = self.a + self.b
+        if n > self.maxValue:
+            raise StopIteration
+        self.a = self.b
+        print("self.a : ",self.a," self.b : ",self.b)
+        self.b = n
+        print("self.a : ",self.a," n : ",n)
+        return n
+
+if __name__ =="__main__":
+    for n in FibIterator():
+        print(n,end=" ")
+            
