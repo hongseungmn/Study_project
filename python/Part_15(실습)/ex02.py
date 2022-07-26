dd4a71c8608175fc7e5f5227bc4c6e7e7027759b
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-26 20:20:57 +0900

    Update ex02.py

--- python/Part_15(실습)/ex02.py
+++ python/Part_15(실습)/ex02.py
@@ -6,7 +6,11 @@ list1_iter = iter(list1)
 #list1 = list1.__iter__()
 
 # __next__()이용하여 값을 가져오도록 한다
+print("결과값 : ",list1_iter.__getattribute__)
 print("결과값 : ",list1_iter.__next__())
 print("결과값 : ",list1_iter.__next__())
 print("결과값 : ",list1_iter.__next__())
 print("결과값 : ",list1_iter.__next__())
+# 더이상 꺼내올 값이 없으므로 StopIteration 예외가 발생한다
+
+
