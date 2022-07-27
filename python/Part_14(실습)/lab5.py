5df38738c23fc5f2f585e05a035e6638a7b228dd
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-07-27 10:39:04 +0900

    Revert "Update lab5.py"
    This reverts commit eca275b22931a3aff287f3474713ca0c2a494c5f.

--- python/Part_14(실습)/lab5.py
+++ python/Part_14(실습)/lab5.py
@@ -1,27 +1,3 @@
 #클래스 2개를 만드는데 먼저 Figure 클래스를 만들고 생성자에서 Width와 Height를 받아서 초기화한다
 #그다음 Quadrangle 클래스를 만들어 출력
 
-class Figure(object):
-    def __init__(self,width,height):
-        self.width = width
-        self.height = height
-class Quadrangle(object):
-    def __init__(self,width,height):
-        self.width = width
-        self.height = height
-
-    #연산자 + 중복 정의
-    def __add__(self,other):
-        return Quadrangle(self.width + other.width,self.height+ other.height)
-    
-
-quad = Quadrangle(2,3)
-figure = Figure(3,4)
-print("너비의 합 : ",quad.width + figure.width)
-print("높이의 합 : ",quad.height + figure.height)
-
-sumQuad = quad + figure
-
-print("합한 도형(너비,높이) :",sumQuad.width,",",sumQuad.height )
-
-    
